bdea1a0b3144694e109956c99efee85412eaba74
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-23 23:02:40 +0300

    testing if my uselist saves password

--- test.py
+++ test.py
@@ -1,2 +1,33 @@
 import unittest
-from user import user
+from user import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviors.
+
+    Args:
+        unittest.TestCase:helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Function to create a user account before each test
+        '''
+        self.new_user = User('Dan', '1234')
+
+    def test_init_(self):
+        '''
+        Test to if check the initialization/creation of user instances is properly done
+        '''
+        self.assertEqual(self.new_user.username, 'Dan')
+        self.assertEqual(self.new_user.password, '1234')
+
+    def test_save_user(self):
+        '''
+        Test to check if the new users info is saved into the users list
+        '''
+        self.new_user.save_User()
+        self.assertEqual(len(User.user_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- user.py
+++ user.py
@@ -1,19 +1,19 @@
-class user:
-    """"
+class User:
+    '''
     class to create user accounts and save their information
-    """
-    Account_list=[]
+    '''
+    user_list=[]
 
-def __init__(self,username,password):
-    """
-    method to define the properties for each user objective will hold
-    """
-    #instance variables
-    self.username = username
-    self.password = password
+    def __init__(self,username,password):
+        '''
+        method to define the properties for each user objective will hold
+        '''
+        #instance variables
+        self.username = username
+        self.password = password
 
-def save_User(self):
-    """
-    Function to save a newly created instance
-    """
-    user.Account_list.append(self)
+    def save_User(self):
+        '''
+        Function to save a newly created instance
+        '''
+        User.user_list.append(self)
