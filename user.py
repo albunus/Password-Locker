823f9d5bd19395aae2e1bf54c5bfe7158a6c3bea
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-23 20:48:13 +0300

    create incidence

--- user.py
+++ user.py
@@ -2,3 +2,18 @@ class user:
     """"
     class to create user accounts and save their information
     """
+    Account_list=[]
+
+def __init__(self,username,password):
+    """
+    method to define the properties for each user objective will hold
+    """
+    #instance variables
+    self.username = username
+    self.password = password
+
+def save_User(self):
+    """
+    Function to save a newly created instance
+    """
+    user.Account_list.append(self)
