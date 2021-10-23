d39bbe28b4fdbc0ee4e4935a57dd85d0afb6a5b2
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-23 23:44:18 +0300

    update my files

--- test.py
+++ test.py
@@ -12,13 +12,15 @@ class TestUser(unittest.TestCase):
         '''
         Function to create a user account before each test
         '''
-        self.new_user = User('Dan', '1234')
+        self.new_user = User('Dan','tush','tushy','1234')
 
     def test_init_(self):
         '''
         Test to if check the initialization/creation of user instances is properly done
         '''
-        self.assertEqual(self.new_user.username, 'Dan')
+        self.assertEqual(self.new_user.first_name, 'Dan')
+        self.assertEqual(self.new_user.last_name, 'tush')
+        self.assertEqual(self.new_user.username, 'tushy')
         self.assertEqual(self.new_user.password, '1234')
 
     def test_save_user(self):
@@ -27,6 +29,42 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_User()
         self.assertEqual(len(User.user_list), 1)
+
+    def test_delete_user(self):
+        """
+        check whether the user was removed from the user accounts list
+        """
+        self.assertEqual(len(User.user_list), 0)
+        self.new_user.save_User()
+        self.assertEqual(len(User.user_list), 1)
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list), 0)
+
+    @classmethod
+    def find_user(cls, username):
+        """Find user by username"""
+        for user in cls.user_list:
+            if user.username == username:
+                return user
+
+    @classmethod
+    def user_exist(cls, username):
+        #Check if user exists
+        for user in cls.user_list:
+            if user.username == username:
+                return True
+            return False
+
+    @classmethod
+    def check_user(cls, username, password):
+        """
+        Check if user exists and if password is correct
+        """
+        user = cls.find_user(username)
+        if user and user.password == password:
+            return True
+        return False
+
 
 
 if __name__ == '__main__':
--- user.py
+++ user.py
@@ -4,11 +4,12 @@ class User:
     '''
     user_list=[]
 
-    def __init__(self,username,password):
-        '''
-        method to define the properties for each user objective will hold
-        '''
-        #instance variables
+    def __init__(self, first_name, last_name, username, password):
+        """
+        Initialize the user.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
         self.username = username
         self.password = password
 
@@ -17,3 +18,21 @@ class User:
         Function to save a newly created instance
         '''
         User.user_list.append(self)
+    def delete_user(self):
+        '''
+        Delete user from the list
+        '''
+        User.user_list.remove(self)
+
+    def test_find_user(self):
+        """
+        check whether the user account exists in the user list
+        """
+        self.found_user = User.find_user("tushy")
+
+    def test_user_exists(self):
+        """
+        check whether the user account exists in the user list
+        """
+        self.found_user = User.user_exist("tushy")
+
