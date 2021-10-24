01b1cef387ab9552920a1c2aa1a0c4b18d8f6bdf
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-24 09:39:33 +0300

    creating instance for the class

--- credentials.py
+++ credentials.py
@@ -9,3 +9,6 @@ class Credentials:
         '''
         initializing social platforms accounts
         '''
+        self.social_platforms_accounts = social_platforms_accounts
+        self.user_accounts_username = user_account_username
+        self.user_accounts_password = user_account_password
